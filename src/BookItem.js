b0f962629b314fc13d3f99589fef6b610402b452
Ngaremaina <ngaremaina4@gmail.com>
2023-04-27 21:00:24 +0300

    Show Book Details

--- src/BookItem.js
+++ src/BookItem.js
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 function BookItem({id, title, price, author, cover}){
 
     function downloadbook(){
-        fetch('SamplePDF.pdf').then(response => {
+        fetch('Book.pdf').then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
                 // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
-                alink.download = 'SamplePDF.pdf';
+                alink.download = 'Book.pdf';
                 alink.click();
             })
         })
@@ -30,7 +30,7 @@ function BookItem({id, title, price, author, cover}){
                 <Link className="btn btn-primary mb-5" to={`/${id}`}>Show details</Link>
                 <div class="card-footer mt-6">
                     <div>
-                    <Button type="button" style={{width: "80%"}} className="btn btn-primary position-absolute bottom-0 start-50 translate-middle-x mb-2" onClick={downloadbook}>Download Book</Button>
+                    <Button type="button" style={{width: "90%"}} className="btn btn-primary position-absolute bottom-0 start-50 translate-middle-x mb-2" onClick={downloadbook}>Download Book</Button>
 
                     </div>
                     
