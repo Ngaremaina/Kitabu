d47232020072805b33d28dbb9365067c89a1720b
Ngaremaina <ngaremaina4@gmail.com>
2023-05-08 11:41:06 +0300

    Completed Application

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer(){
                     
                     
                 </ul>
-                <p class="text-center text-white justify-content-center" style={{marginLeft:"50px", marginRight:"50px"}}>Kitabu is a web application that shows the user a collection of books. The user can view a list of their favorite books along with information about them, such the author, summary, price, and publisher, to mention a few. Also, the user can add their own book to the app and view it listed with all the other books. Also, the user has the option to update their own book by adding or deleting any elements they see fit. Also, if a person doesn't want to see the book, they can delete it. The user may also search for their preferred book by its title, such as Harry Potter.</p>
+                <p class="text-center justify-content-center" style={{marginLeft:"50px", marginRight:"50px"}}>Kitabu is a web application that shows the user a collection of books. The user can view a list of their favorite books along with information about them, such the author, summary, price, and publisher, to mention a few. Also, the user can add their own book to the app and view it listed with all the other books. Also, the user has the option to update their own book by adding or deleting any elements they see fit. Also, if a person doesn't want to see the book, they can delete it. The user may also search for their preferred book by its title, such as Harry Potter.</p>
                 <p class="text-center text-body-secondary">© 2023 Vitabu</p>
             </footer>
        
