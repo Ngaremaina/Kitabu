327f21e08c50f110b640b754adb1ad3469832bdc
Ngare Maina <45742522+Ngaremaina@users.noreply.github.com>
2025-05-26 14:37:24 +0300

    :sparkles: Updates on the UI - Added Loader and Vercel analytics

--- src/App.js
+++ src/App.js
@@ -8,21 +8,27 @@ import BookDetail from './components/BookDetail';
 import EditBook from './components/EditBook';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import { CirclesWithBar } from "react-loader-spinner"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [books, setBook] = useState([])
+  const [loading, setLoading] = useState(false)
  
   //Fetching data from json file
   useEffect(() => {
-    const interval = setInterval(() => {
+    setLoading(true)
+    try{
       fetch("https://book-data.onrender.com/books")
       .then(res => res.json())
       .then(data => setBook(data))
-        
-      .catch(err => console.error(err));
-    }, 6000);
-  
-    return () => clearInterval(interval);
+    }
+    catch(error){
+      console.log(error)
+    }
+    finally{
+      setLoading(false)
+    }
   }, []);
 
   //adding book to the server
@@ -38,19 +44,38 @@ function App() {
 
   return (
     //rendering the components
-    <div className='mybooks'>
-       <Header searchBook = {searchBook}/>
-       {/*rendering the routes to the components*/}
-      <Routes>
-        <Route exact path='/' element={ <BookList books={books}/>}></Route>
-        <Route path='/addbook' element={<NewBook addBook={addBook}/>}></Route>
-        <Route path='/:id' element={<BookDetail />}></Route>
-        <Route path='/editbook/:id' element={<EditBook addBook={addBook}/>}></Route>
-        <Route path='/contact us' element={<Contact />}></Route>
-      </Routes>
-      <Footer />
-  
-    </div>
+    <>
+    {loading ? (
+      <CirclesWithBar
+        height="100"
+        width="100"
+        color="#4fa94d"
+        outerCircleColor="#4fa94d"
+        innerCircleColor="#4fa94d"
+        barColor="#4fa94d"
+        ariaLabel="circles-with-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass="loader"
+        visible={true}
+      />
+    ) 
+      : (
+        <div className='mybooks'>
+          <Analytics/>
+          <Header searchBook = {searchBook}/>
+          {/*rendering the routes to the components*/}
+          <Routes>
+            <Route exact path='/' element={ <BookList books={books}/>}></Route>
+            <Route path='/addbook' element={<NewBook addBook={addBook}/>}></Route>
+            <Route path='/:id' element={<BookDetail />}></Route>
+            <Route path='/editbook/:id' element={<EditBook addBook={addBook}/>}></Route>
+            <Route path='/contact us' element={<Contact />}></Route>
+          </Routes>
+          <Footer />
+      
+        </div>)
+      }
+      </>
   );
 }
 
